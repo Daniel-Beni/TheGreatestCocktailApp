500061c8e289051ed251f5611b1953f278349d5c
Daniel Beni Niyobuzima <daniel-beni.niyobuzima@isen.yncrea.fr>
2026-02-18 19:51:14 +0100

    Section 1 : exploration composants Compose + cycle de vie Activity

--- app/src/main/java/fr/isen/danielbeni/thegreatestcocktailapp/MainActivity.kt
+++ app/src/main/java/fr/isen/danielbeni/thegreatestcocktailapp/MainActivity.kt
@@ -1,4 +1,6 @@
 package fr.isen.danielbeni.thegreatestcocktailapp
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -28,19 +30,58 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    override fun onStart() {
+        super.onStart()
+        // activity devient visible
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //activity est interactive
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // activity perd le focus
+    }
+
+    override fun onStop() {
+        super.onStop()
+        // activity n'est plus visible
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        // activity reprend depuis stopped
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // activity est détruite
+    }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    // alignement horizontal
+    Row (modifier = modifier) {
+            Text("Hello $name!")
+            Text("Hello Isen")
+
+    }
+    // alignement vertical
+
+    Column(modifier = modifier){
+       Text("Hello $name!")
+        Text("Hello Isen")
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
+
     TheGreatestCocktailAppTheme {
         Greeting("Android")
     }
